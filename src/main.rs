ba4ecad3dcbae3fcf6fb02c29f52cec750d61dfb
Heinrich Malan <heinrichmalan@hotmail.com>
2020-03-06 20:55:07 +0100

    added users api route

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,43 @@
-use actix_files as fs;
-use actix_web::{ web, App, HttpRequest, HttpServer, HttpResponse };
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
+use serde::{Deserialize, Serialize};
 
 extern crate listenfd;
-use listenfd::ListenFd;
 use dotenv;
+use listenfd::ListenFd;
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    name: String,
+    email: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Users {
+    users: Vec<User>,
+}
+
+async fn get_users() -> HttpResponse {
+    let mut user_list = Vec::<User>::new();
+    for i in 1..10 {
+        user_list.push(User {
+            name: i.to_string(),
+            email: format!("{}@email.com", i.to_string()),
+        })
+    }
+    HttpResponse::Ok().json(user_list)
+}
 
 #[actix_rt::main]
-async fn main()  {
+async fn main() {
     let mut listenfd = ListenFd::from_env();
     let key = "RUST_SPA_PORT";
     let port = dotenv::var(key).unwrap();
     let mut server = HttpServer::new(|| {
-        App::new()
-        .service(web::scope("/api")
-                .route("/test", web::get().to(|| HttpResponse::Ok().body("test"))))
+        App::new().service(
+            web::scope("/api")
+                .route("/test", web::get().to(|| HttpResponse::Ok().body("test")))
+                .route("/users", web::get().to(get_users)),
+        )
     });
 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
