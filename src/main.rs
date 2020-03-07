e626a8cfa3d5d944f422a793d817cba1d5ecedfa
Heinrich Malan <heinrich@heinbio.me>
2020-03-07 15:19:54 +0100

    Added random name generation for user list

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
 use serde::{Deserialize, Serialize};
-
+use rand::Rng;
 extern crate listenfd;
 use dotenv;
 use listenfd::ListenFd;
@@ -16,12 +16,43 @@ struct Users {
     users: Vec<User>,
 }
 
+struct Name {
+    first_name: String,
+    last_name: String
+}
+
+fn gen_random_name() -> Name {
+    let mut rng = rand::thread_rng();
+    let male_names = vec!["Alex", "Friedrich", "Karl", "John", "Johan"];
+    let female_names = vec!["Igrit", "Brigitte", "Olga", "Helga"];
+    let random_index: usize = rng.gen_range(0, male_names.len() - 1);
+    let last_name_root = male_names[random_index];
+    let mut last_name: String;
+    let mut first_name: String;
+    let male_or_female: usize = rng.gen_range(0, 2);
+    if male_or_female == 0 {
+        let first_name_index: usize = rng.gen_range(0, female_names.len() - 1);
+        first_name = String::from(female_names[first_name_index]);
+        last_name = format!("{}sdottir", last_name_root);
+    } else {
+        let first_name_index: usize = rng.gen_range(0, female_names.len() - 1);
+        first_name = String::from(male_names[first_name_index]);
+        last_name = format!("{}sson", last_name_root);
+    }
+    
+    return Name {
+        first_name,
+        last_name
+    }
+}
+
 async fn get_users() -> HttpResponse {
     let mut user_list = Vec::<User>::new();
-    for i in 1..10 {
+    for i in 0..10 {
+        let name = gen_random_name();
         user_list.push(User {
-            name: i.to_string(),
-            email: format!("{}@email.com", i.to_string()),
+            name: format!("{} {}", name.first_name, name.last_name),
+            email: format!("{}.{}@email.com", name.first_name, name.last_name),
         })
     }
     HttpResponse::Ok().json(user_list)
