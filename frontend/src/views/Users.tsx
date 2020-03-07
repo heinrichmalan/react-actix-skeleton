7c84d509e0cb7de203050c1dcdb33eab69a8d5e8
Heinrich Malan <heinrich@heinbio.me>
2020-03-07 15:22:20 +0100

    Factored out user table added spinner and artificial delay

--- frontend/src/views/Users.tsx
+++ frontend/src/views/Users.tsx
@@ -1,50 +1,79 @@
 import React, { Component } from "react";
-
+import Spinner from "../components/Spinner";
 import "../App.css";
 
-class UserListing extends Component<Object, { users: Array<any> }> {
-    constructor(props: Readonly<Object>) {
-        super(props);
+const waitNSecs = (seconds: number) => {
+  let promise = new Promise((resolve, _) => {
+    setTimeout(() => {
+      resolve();
+    }, seconds * 1000);
+  });
 
-        this.state = {
-            users: []
-        };
-    }
+  return promise;
+};
 
-    componentDidMount = async () => {
-        try {
-            const url = "/api/users";
-            const res = await fetch(url);
-            const users: Array<any> = await res.json();
+type UserInfo = { name: string; email: string };
 
-            this.setState({ users });
-        } catch (err) {
-            console.error(err);
-        }
+const UserTable = ({ users }: { users: Array<UserInfo> }) => (
+  <table>
+    <tr>
+      <th style={{ textAlign: "left" }}>Name</th>
+      <th style={{ textAlign: "left", paddingLeft: "10px" }}>Email</th>
+    </tr>
+    {users.map((u: UserInfo) => {
+      return (
+        <tr>
+          <td style={{ textAlign: "left" }}>{u.name}</td>
+          <td style={{ textAlign: "left", paddingLeft: "10px" }}>{u.email}</td>
+        </tr>
+      );
+    })}
+  </table>
+);
+
+type UserListingState = {
+  users: Array<any>;
+  loading: boolean;
+};
+
+class UserListing extends Component<Object, UserListingState> {
+  constructor(props: Readonly<Object>) {
+    super(props);
+
+    this.state = {
+      users: [],
+      loading: true
     };
+  }
 
-    render() {
-        return (
-            <div
-                className="App-header"
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    margin: "0 auto"
-                }}
-            >
-                {this.state.users.map(u => {
-                    return (
-                        <div style={{ width: "auto", margin: "0 auto" }}>
-                            Name: {u.name}
-                            <br />
-                            Email: {u.email}
-                        </div>
-                    );
-                })}
-            </div>
-        );
+  componentDidMount = async () => {
+    try {
+      const p = waitNSecs(1.5);
+      const url = "/api/users";
+      const res = await fetch(url);
+      const users: Array<any> = await res.json();
+      await p;
+      this.setState({ users, loading: false });
+    } catch (err) {
+      console.error(err);
     }
+  };
+
+  render() {
+    const { loading, users } = this.state;
+    return (
+      <div
+        className="App-header"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          margin: "0 auto"
+        }}
+      >
+        {!loading ? <UserTable users={users} /> : <Spinner />}
+      </div>
+    );
+  }
 }
 
 export default UserListing;
